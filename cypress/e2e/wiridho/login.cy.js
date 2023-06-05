ee0cfeddbf132bfa0731212e49c1805731cd0b09
wiridho <tambunanwiridho@gmail.com>
2023-06-05 11:12:25 +0700

    Login Functionality

---
+++ cypress/e2e/wiridho/login.cy.js
@@ -0,0 +1,35 @@
+import baseLogin from "../../support/PageObject/loginPage.cy";
+const loginData = require("../../fixtures/orangeHRM/login.json");
+
+describe("Login Functionality", () => {
+  const BASE_LOGIN = new baseLogin();
+  context("Login Success", () => {
+    it("TC-D01 : Sukses Login dengan valid credential", () => {
+      cy.visit(
+        "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+      );
+      BASE_LOGIN.inputEmail(loginData.validEmail);
+      BASE_LOGIN.inputPassword(loginData.validPassword);
+      BASE_LOGIN.clickLogin();
+      BASE_LOGIN.verifyDashboard();
+    });
+  });
+  context("Login Failure", () => {
+    it("TC-D02 : Login gagal (Wrong username)", () => {
+      cy.visit(
+        "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+      );
+      BASE_LOGIN.inputEmail(loginData.wrongEmail);
+      BASE_LOGIN.inputPassword(loginData.validPassword);
+      BASE_LOGIN.clickLogin();
+      BASE_LOGIN.showErrorAlert();
+    });
+    it("TC-D03 : Login gagal (Input blank data)", () => {
+      cy.visit(
+        "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+      );
+      BASE_LOGIN.clickLogin();
+      BASE_LOGIN.showformValidation();
+    });
+  });
+});
---
+++ cypress/support/PageObject/loginPage.cy.js
@@ -0,0 +1,32 @@
+const loginData = require("../../fixtures/orangeHRM/login.json");
+
+class baseLogin {
+  email = ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input";
+  password = ":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input";
+  loginBtn = ".oxd-button";
+  dashboardImg = ".oxd-topbar-header-breadcrumb > .oxd-text";
+  alertError = ".oxd-alert";
+  formValidation = ":nth-child(2) > .oxd-input-group > .oxd-text";
+
+  inputEmail(email) {
+    cy.get(this.email).type(email);
+  }
+  inputPassword(password) {
+    cy.get(this.password).type(password);
+  }
+  clickLogin() {
+    cy.get(this.loginBtn, { timeout: 2000 }).click();
+  }
+  verifyDashboard() {
+    cy.contains("Dashboard");
+    cy.get(this.dashboardImg).should("be.visible");
+  }
+  showErrorAlert() {
+    cy.get(this.alertError).should("be.visible");
+  }
+  showformValidation() {
+    cy.get(this.formValidation).should("contain.text", "Required");
+  }
+}
+
+export default baseLogin;
