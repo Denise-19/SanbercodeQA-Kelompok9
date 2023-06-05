10574898df779ea48761af225b0f7ebc6ecb171f
UmbuReinhard <rambucyntiaverolita@gmail.com>
2023-06-05 15:46:37 +0700

    Update5

--- cypress/e2e/Rambu/Admin_Organization_EditLocation.cy.js
+++ cypress/e2e/Rambu/Admin_Organization_EditLocation.cy.js
@@ -20,7 +20,7 @@ describe('Organization-Edit Location', () => {
 		cy.get('.oxd-table-filter-header-title > .oxd-text').should('contain','Locations');
 
 		// Edit Location
-		cy.get(':nth-child(11) > .oxd-table-row > :nth-child(7) > .oxd-table-cell-actions > :nth-child(2) > .oxd-icon').click({force:true}); // click Edit 
+		cy.get(':nth-child(5) > .oxd-table-row > :nth-child(7) > .oxd-table-cell-actions > :nth-child(2) > .oxd-icon').click({force:true}); // click Edit 
 		cy.get(':nth-child(1) > .oxd-grid-2 > .oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').type('edit');
 		cy.get('.oxd-button--secondary').click(); //click Save Button
 		cy.get('.oxd-toast').should('contain','Successfully Updated');
