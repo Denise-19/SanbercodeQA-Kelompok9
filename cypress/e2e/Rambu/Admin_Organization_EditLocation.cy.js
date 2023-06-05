59efe88849e99c2f1026bfefd5b835fa1fb0e148
UmbuReinhard <rambucyntiaverolita@gmail.com>
2023-06-05 08:29:11 +0700

    Updated

--- cypress/e2e/Rambu/Admin_Organization_EditLocation.cy.js
+++ cypress/e2e/Rambu/Admin_Organization_EditLocation.cy.js
@@ -36,7 +36,7 @@ describe('TC0_B07 : Successfully Edit Location', () => {
 		// Go to Location menu
 		cy.get(':nth-child(1) > .oxd-main-menu-item').click() //click menu Admin
 		cy.get(':nth-child(3) > .oxd-topbar-body-nav-tab-item').click() //click Menu Organization
-		cy.get('.oxd-dropdown-menu > :nth-child(2)').click() //click Sub Menu Locations
+		cy.get('.oxd-dropdown-menu > :nth-child(2)').click() //click Sub Menu Location
 		cy.get('.oxd-table-filter-header-title > .oxd-text').should('contain','Locations')
 		
 		// Edit Location
