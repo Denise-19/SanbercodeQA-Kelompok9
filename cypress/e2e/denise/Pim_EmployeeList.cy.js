5b1452d6362924ed01020e15412cca6b26c212d5
Denise.Aruan@intikom.co.id <Denise.Aruan@intikom.co.id>
2023-06-05 13:45:05 +0700

    Search Employee

--- cypress/e2e/denise/Pim_EmployeeList.cy.js
+++ cypress/e2e/denise/Pim_EmployeeList.cy.js
@@ -16,4 +16,23 @@ describe('TB02_03: List Employee', () => {
 	})
   })
 
- 
+  describe('TB02_04: Search Employee', () => {
+	it('passes', () => { 
+		// Visit the URL
+		cy.visit("https://opensource-demo.orangehrmlive.com/");
+		
+		// Log in as admin
+		cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin');
+		cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123');
+		cy.get('.oxd-button').click()
+
+		// Go to PIM menu
+		cy.get(':nth-child(2) > .oxd-main-menu-item').click()
+        cy.get(':nth-child(2) > .oxd-input').type('EMP0001');
+        cy.get('.oxd-form-actions > .oxd-button--secondary').click({ force: true });
+        
+		// Input Country
+		
+	})
+  })
+  
